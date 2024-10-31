b56a98c4a843b1874586040cfaefc72f18e0ef8c
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-10-31 22:30:18 +0300

    Time: 0 ms (100%), Space: 45.6 MB (20.96%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public void SortColors(int[] nums) {
+        int temp=0;
+        for(int i=0;i<nums.Length-1;i++)
+        {
+            for(int j=i+1;j<nums.Length;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        
+    }
+}
