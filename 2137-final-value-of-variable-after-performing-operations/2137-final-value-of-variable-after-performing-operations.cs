7c402f7799a396bffa41d2696496f63987abe1f0
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-10-31 22:20:06 +0300

    Time: 0 ms (100%), Space: 41.5 MB (35.05%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int FinalValueAfterOperations(string[] operations) {
+        int X=0;
+
+        for(int i=0;i<operations.Length;i++)
+        {
+            if( (operations[i]=="++X")|| (operations[i]=="X++") )
+            {
+                X++;
+
+            }else if((operations[i]=="--X")|| (operations[i]=="X--"))
+            {
+                X--;
+            }
+
+        }
+
+
+        return X;
+        
+    }
+}
