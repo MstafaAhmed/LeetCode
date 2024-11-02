572b90bd1a52c887448ab2b881b0380eef82aa3d
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-11-02 11:36:22 +0200

    Time: 0 ms (100%), Space: 42.8 MB (94.35%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool ThreeConsecutiveOdds(int[] arr) {
+      //  if(arr.Length<3){return false;}
+        int odd=0;
+        for(int i=0;i<arr.Length;i++)
+        {
+            if(arr[i]%2==1)
+            {
+                odd++;
+            }else
+            {
+                odd=0;
+            }
+            if(odd==3)
+            {
+                break;
+            }
+
+        }
+        if(odd==3)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+        
+    }
+}
