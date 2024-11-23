404d6a5fac4a1f9eca075a257c4278d0edd1e4f2
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-11-23 10:44:44 +0200

    Time: 0 ms (100%), Space: 27.2 MB (20.66%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int TheMaximumAchievableX(int num, int t) {
+        int ans=0;
+        if(num>0 && t>=0){ans = num+2*t;}
+    
+
+        
+        return ans;
+    }
+}
