a4e3698072493eefa8714c9286e82a25e7e5114f
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-11-24 11:38:08 +0200

    Time: 0 ms (100%), Space: 26.9 MB (47.26%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int NumberOfSteps(int num) {
+        int ans=0;
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num=num/2;
+                ans++;
+            }else
+            {
+                num-=1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
