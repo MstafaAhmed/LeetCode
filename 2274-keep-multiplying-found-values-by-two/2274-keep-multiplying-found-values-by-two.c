d1c6a3fb88e0920cd83f4b1c8a786602bd4fb020
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2024-12-01 11:01:57 +0200

    Time: 0 ms (100%), Space: 8.6 MB (38.89%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.c
@@ -0,0 +1,18 @@
+int findFinalValue(int* nums, int numsSize, int original) {
+    int found =1;
+
+    while(found==1)
+    {
+        found=0;
+        for(int i=0;i<numsSize;i++)
+        {
+            if(nums[i]==original)
+            {
+                original*=2;
+                found=1;
+                break;
+            }
+        }
+    }
+        return original;   
+}
