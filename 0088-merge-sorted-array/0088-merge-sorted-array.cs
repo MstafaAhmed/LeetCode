3a584b2a5326c3042505e4c5292da3dac4d8e24a
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2025-02-13 11:22:17 +0200

    Time: 1 ms (32.79%), Space: 46.9 MB (35.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cs
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -1,24 +1,10 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-
-         if(n==0){return;}
-        for(int i=0;i<n;i++)
+        for(int i=0 ;i<n ;i++)
         {
             nums1[m+i]=nums2[i];
         }
+        Array.Sort(nums1);
         
-        int temp=0;
-        for(int i=0;i<nums1.Length-1;i++)
-        {
-            for(int j=i+1;j<nums1.Length;j++)
-            {
-                if(nums1[i]>nums1[j])
-                {
-                    temp=nums1[i];
-                    nums1[i]=nums1[j];
-                    nums1[j]=temp;
-                }
-            }
-        }
     }
 }
