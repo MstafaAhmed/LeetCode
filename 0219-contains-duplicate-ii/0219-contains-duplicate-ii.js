f870bcad3526cfe820f10087cc37407d714f353c
Mostafa Ahmed <mostafaabdelfattah2024@gmail.com>
2025-11-02 16:41:57 +0200

    Time: 29 ms (45.41%), Space: 81.2 MB (30.93%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -1,18 +1,11 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
- */
 var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
     
-    for (let i=0;i< nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]==nums[j]){
-               if (Math.abs(i-j) <=k){
-                return true;
-               }
-            }
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+            return true;
         }
+        map.set(nums[i], i);
     }
     return false;
 };
